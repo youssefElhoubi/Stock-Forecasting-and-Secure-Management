99cf7e4aeb312199ca083f9bed866f5317c439c0
youssefElhoubi <elhoubiyoussef@gmail.com>
2025-12-22 21:52:11 +0100

    complated user service interface

--- src/main/java/com/STFAS/dto/auth/request/SignUpRequestDto.java
+++ src/main/java/com/STFAS/dto/auth/request/SignUpRequestDto.java
@@ -1,7 +1,9 @@
 package com.STFAS.dto.auth.request;
 
+import com.STFAS.enums.Role;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,12 +14,18 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class SignUpRequestDto {
-    @NotBlank(message = "name is required")
+    @NotBlank(message = "Name is required")
     private String name;
+
     @NotBlank(message = "Email is required")
     @Email(message = "Invalid email format")
     private String email;
 
     @NotBlank(message = "Password is required")
+    @Size(min = 6, message = "Password must be at least 6 characters long")
     private String password;
+
+    // Optional: You might want to default this to USER in your service logic
+    // instead of letting the user pick it here.
+    private Role role;
 }
--- src/main/java/com/STFAS/service/repository/Auth.java
+++ src/main/java/com/STFAS/service/repository/Auth.java
@@ -1,4 +1,12 @@
 package com.STFAS.service.repository;
 
+import com.STFAS.dto.auth.request.AuthRequestDto;
+import com.STFAS.dto.auth.request.SignUpRequestDto;
+import com.STFAS.dto.auth.response.AuthResponseDto;
+import com.STFAS.dto.auth.response.UserInfoDto;
+
 public interface Auth {
+    AuthResponseDto login(AuthRequestDto authRequestDto);
+    AuthResponseDto signup(SignUpRequestDto authRequestDto);
+    UserInfoDto  getUserInfo(String id);
 }
