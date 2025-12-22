2b92155440be9967de1ffb5d4e4bbc1d81981756
youssefElhoubi <elhoubiyoussef@gmail.com>
2025-12-22 16:16:51 +0100

    started new project and started work on it

---
+++ src/main/java/com/STFAS/StockForecastingAndSecureManagementApplication.java
@@ -0,0 +1,13 @@
+package com.STFAS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockForecastingAndSecureManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockForecastingAndSecureManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/STFAS/StockForecastingAndSecureManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.STFAS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockForecastingAndSecureManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
