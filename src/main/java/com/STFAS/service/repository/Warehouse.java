8cf09c632d27fd09404dc076593416bb21985cd2
youssefElhoubi <elhoubiyoussef@gmail.com>
2025-12-23 15:01:58 +0100

    created the warehouser service interface

---
+++ src/main/java/com/STFAS/service/repository/Warehouse.java
@@ -0,0 +1,15 @@
+package com.STFAS.service.repository;
+
+import com.STFAS.dto.Warehouse.request.WarehouseRequestDto;
+import com.STFAS.dto.Warehouse.request.WarehouseUpdateRequest;
+import com.STFAS.dto.Warehouse.response.WarehouseResponseDto;
+
+import java.util.List;
+
+public interface Warehouse {
+    WarehouseResponseDto createWarehouse(WarehouseRequestDto request);
+    WarehouseResponseDto updateWarehouse(String id, WarehouseUpdateRequest request);
+    WarehouseResponseDto getWarehouseById(String id);
+    List<WarehouseResponseDto> getAllWarehouses();
+    void deleteWarehouse(String id);
+}
