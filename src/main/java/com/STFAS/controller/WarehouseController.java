058497f24fd1fac58e138331ff44046b8b91b353
youssefElhoubi <elhoubiyoussef@gmail.com>
2025-12-23 16:21:06 +0100

    create most of controller methods

--- src/main/java/com/STFAS/controller/WarehouseController.java
+++ src/main/java/com/STFAS/controller/WarehouseController.java
@@ -1,23 +1,35 @@
 package com.STFAS.controller;
 
 import com.STFAS.dto.Warehouse.request.WarehouseRequestDto;
+import com.STFAS.dto.Warehouse.request.WarehouseUpdateRequest;
 import com.STFAS.dto.Warehouse.response.WarehouseResponseDto;
 import com.STFAS.entity.Warehouse;
 import com.STFAS.service.WarehouseService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("api/warehouse")
-public class WarehouseController{
+public class WarehouseController {
     private final WarehouseService warehouseService;
 
-    public ResponseEntity<WarehouseResponseDto> Create(@RequestBody WarehouseRequestDto warehouse){
+    @PostMapping
+    public ResponseEntity<WarehouseResponseDto> create(@RequestBody WarehouseRequestDto warehouse) {
         return ResponseEntity.ok(warehouseService.createWarehouse(warehouse));
     }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<WarehouseResponseDto> update(@PathVariable String id, @RequestBody WarehouseUpdateRequest warehouse) {
+        return ResponseEntity.ok(warehouseService.updateWarehouse(id, warehouse));
+    }
+    @GetMapping("all")
+    public ResponseEntity<List<WarehouseResponseDto>> all(){
+        return ResponseEntity.ok(warehouseService.getAllWarehouses());
+    }
+
 
 }
