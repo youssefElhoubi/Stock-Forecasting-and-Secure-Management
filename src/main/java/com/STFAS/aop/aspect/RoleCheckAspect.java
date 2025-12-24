4824633be2288d903f334f61dbfe2ed1693750d2
youssefElhoubi <elhoubiyoussef@gmail.com>
2025-12-24 14:41:41 +0100

    fixed docker and security problems befor strting the work on the anotation

--- src/main/java/com/STFAS/aop/aspect/RoleCheckAspect.java
+++ src/main/java/com/STFAS/aop/aspect/RoleCheckAspect.java
@@ -1,7 +1,9 @@
 package com.STFAS.aop.aspect;
 
+import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
 
 @Component
+@Aspect
 public class RoleCheckAspect {
 }
