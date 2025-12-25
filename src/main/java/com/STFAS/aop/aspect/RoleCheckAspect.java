39babd1db9eeba04d7bba70c636856d7f2fb0be2
youssefElhoubi <elhoubiyoussef@gmail.com>
2025-12-25 09:50:20 +0100

    inserted the jwt tools in the wanted annotation

--- src/main/java/com/STFAS/aop/aspect/RoleCheckAspect.java
+++ src/main/java/com/STFAS/aop/aspect/RoleCheckAspect.java
@@ -1,10 +1,17 @@
 package com.STFAS.aop.aspect;
 
+import com.STFAS.security.CustomUserDetailsService;
+import com.STFAS.security.JwtUtils;
+import lombok.RequiredArgsConstructor;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
 
 @Component
 @Aspect
+@RequiredArgsConstructor
 public class RoleCheckAspect {
+    private final CustomUserDetailsService customUserDetailsService;
+    private final JwtUtils jwtUtils;
+
 
 }
