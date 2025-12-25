3d04fb189ad203b026b18ac2c4f24d567fde3ba3
youssefElhoubi <elhoubiyoussef@gmail.com>
2025-12-25 10:53:16 +0100

    Refactored RoleCheckAspect to include JWT token validation and parameter checking

--- src/main/java/com/STFAS/aop/interfaces/HaveAccess.java
+++ src/main/java/com/STFAS/aop/interfaces/HaveAccess.java
@@ -7,5 +7,6 @@ import java.lang.annotation.Target;
 
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
-public @interface roleValidation {
+public @interface HaveAccess {
+    String id() default "";
 }
